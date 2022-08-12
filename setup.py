e32dba5f867e37d2d7ff7c18911a9fce03d9e6d4
Chenxu <nkcxliu@gmail.com>
2022-08-12 17:14:30 -0400

    Modify setup.py

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
     cmdclass=versioneer.get_cmdclass(),
     license='MIT',
 
+
     # Which Python importable modules should be included when your package is installed
     # Handled automatically by setuptools. Use 'exclude' to prevent some specific
     # subpackage(s) from being added, if needed
